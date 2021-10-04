a5159bed2a0f054c1172781444ddbc697b3552a4
Nisheed Jagadish <njagadish@microsoft.com>
2021-10-04 09:15:07 +0530

    Working example

--- OnAir.ino
+++ OnAir.ino
@@ -8,19 +8,20 @@
 #include <ArduinoJson.h>
 
 // Pin Definitions
-#define SDFILE_PIN_CS  4
+#define SDFILE_PIN_CS D3
+#define LED D4
+/**
+    Constants
+*/
+const char FILE_NAME[] = "config.txt";
+const char CLIENT_ID[] = "d25a7a02-b95a-45c7-80c4-8a2cac95c002";
+const char TENANT_ID[] = "777fd943-bc3b-456c-afff-41f772edc972";
+const char SCOPES[] = "&scope=user.read%20presence.read%20offline_access";
+const int POLLING_INTERVAL = 15;
 
-// Default file
-#define SDFILE_NAME "config.txt"
-
-
-// Client ID
-const String clientID = "d25a7a02-b95a-45c7-80c4-8a2cac95c002";
-// Tenant ID
-const String tenantID = "777fd943-bc3b-456c-afff-41f772edc972";
-// Scopes Required
-const String scopes = "&scope=user.read%20presence.read%20offline_access";
-
+/**
+   Variables
+*/
 // WIFI username/password
 String ssid = "";
 String password = "";
@@ -35,7 +36,8 @@ String authToken = "";
 String refreshToken = "";
 
 // Loads the configuration from a file
-void loadConfiguration(const char *filename) {
+void loadConfiguration(const char *filename)
+{
 
   // Open file for reading
   File file = SD.open(filename);
@@ -45,7 +47,8 @@ void loadConfiguration(const char *filename) {
   Serial.println("Loading config file...");
 
   // Loop through the file data
-  while (file.available()) {
+  while (file.available())
+  {
     String buffer = file.readStringUntil('\n');
     // Trim the string
     buffer.trim();
@@ -53,17 +56,20 @@ void loadConfiguration(const char *filename) {
     lineNum++;
 
     // Exit from the loop in-case number of line exceeds interest
-    if (lineNum > lineNumsOfInterest) {
+    if (lineNum > lineNumsOfInterest)
+    {
       break;
     }
 
     // On line one should be the username
-    if (lineNum == 1) {
+    if (lineNum == 1)
+    {
       ssid = buffer;
     }
 
     // On line two should be the password
-    if (lineNum == 2) {
+    if (lineNum == 2)
+    {
       password = buffer;
     }
   }
@@ -77,7 +83,8 @@ void loadConfiguration(const char *filename) {
 }
 
 // starts a WIFI connection based on the ssid/password from the SD card
-void startWifiConnection() {
+void startWifiConnection()
+{
   // Explicitly set the ESP8266 to be a WiFi-client
   WiFi.mode(WIFI_STA);
   // Start connecting
@@ -85,7 +92,8 @@ void startWifiConnection() {
 
   Serial.print("Starting WIFI connection...");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -96,14 +104,15 @@ void startWifiConnection() {
   Serial.println(WiFi.localIP());
 }
 
-void getDeviceCode(WiFiClientSecure& client, HTTPClient& http) {
+void getDeviceCode(WiFiClientSecure &client, HTTPClient &http)
+{
+
+  DynamicJsonDocument doc(1024);
 
   // Create request body
-  String body = String("client_id=" + clientID + scopes);
+  String body = "client_id=" + String(CLIENT_ID) + String(SCOPES);
 
-  DynamicJsonDocument doc(768);
-
-  http.begin(client, String("https://login.microsoftonline.com/" + tenantID + "/oauth2/v2.0/devicecode"));
+  http.begin(client, "https://login.microsoftonline.com/" + String(TENANT_ID) + "/oauth2/v2.0/devicecode");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
   Serial.println("Device Code - POST");
@@ -112,15 +121,20 @@ void getDeviceCode(WiFiClientSecure& client, HTTPClient& http) {
   int httpCode = http.POST(body);
 
   // httpCode will be negative on error
-  if (httpCode > 0) {
+  if (httpCode > 0)
+  {
     // HTTP header has been send and Server response header has been handled
-    if (httpCode == HTTP_CODE_OK) {
+    if (httpCode == HTTP_CODE_OK)
+    {
       String payload = http.getString();
       // Deserialize the JSON document
       DeserializationError error = deserializeJson(doc, payload);
-      if (error) {
+      if (error)
+      {
         Serial.printf("Failed to parse payload");
-      } else {
+      }
+      else
+      {
 
         // Use this to make the polling request for auth
         deviceCode = doc["device_code"].as<String>();
@@ -149,62 +163,80 @@ void getDeviceCode(WiFiClientSecure& client, HTTPClient& http) {
 
         waitForUserAuth(client, http);
       }
-    } else {
+    }
+    else
+    {
       // In case some other error code
       Serial.printf("Device Code - POST - Code: %d\n", httpCode);
     }
-  } else {
+  }
+  else
+  {
     Serial.printf("Device Code - POST - Failed!, error: %s\n", http.errorToString(httpCode).c_str());
   }
   http.end();
 }
 
-void waitForUserAuth(WiFiClientSecure& client, HTTPClient& http) {
+void waitForUserAuth(WiFiClientSecure &client, HTTPClient &http)
+{
   static bool isPending = true;
-  String body = String("grant_type=urn:ietf:params:oauth:grant-type:device_code&client_id=" + clientID + "&device_code=" + deviceCode);
+  String body = "grant_type=urn:ietf:params:oauth:grant-type:device_code&client_id=" + String(CLIENT_ID) + "&device_code=" + deviceCode;
   DynamicJsonDocument doc(4096);
 
-  while (expiresIn > 0 && isPending) {
+  while (expiresIn > 0 && isPending)
+  {
     // Delay the calls
     delay(authInterval * 1000);
-    
+
     // Reduce the expires time
     expiresIn -= authInterval;
-    
+
     // Wait for users authtoken
-    http.begin(client, String("https://login.microsoftonline.com/" + tenantID + "/oauth2/v2.0/token"));
+    http.begin(client, "https://login.microsoftonline.com/" + String(TENANT_ID) + "/oauth2/v2.0/token");
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
     Serial.println("Device Auth - POST");
     // start connection and send HTTP header
     int httpCode = http.POST(body);
-    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_BAD_REQUEST) {
+    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_BAD_REQUEST)
+    {
       String payload = http.getString();
       Serial.println(payload);
       // Deserialize the JSON document
       DeserializationError error = deserializeJson(doc, payload);
-      if (error) {
+      if (error)
+      {
         Serial.printf("Failed to parse payload");
         isPending = false;
-      } else {
-        if (doc.containsKey("error")) {
+      }
+      else
+      {
+        if (doc.containsKey("error"))
+        {
           String errorCode = doc["error"].as<String>();
-          if (errorCode != "authorization_pending") {
+          if (errorCode != "authorization_pending")
+          {
             Serial.print("Device Auth - POST - Authorization failed:");
             Serial.println(errorCode);
             isPending = false;
-          } else {
+          }
+          else
+          {
             Serial.print("Device Auth - POST - Authorization pending:");
             Serial.println(errorCode);
           }
-        } else {
+        }
+        else
+        {
           authToken = doc["access_token"].as<String>();
           refreshToken = doc["refresh_token"].as<String>();
           expiresIn = doc["expires_in"].as<int>();
           isPending = false;
         }
       }
-    } else {
+    }
+    else
+    {
       // In case some other error code
       Serial.printf("Device Auth - POST - Code: %d\n", httpCode);
       isPending = false;
@@ -213,50 +245,125 @@ void waitForUserAuth(WiFiClientSecure& client, HTTPClient& http) {
   }
 
   // Check if authtoken and refresh token is set
-  if (authToken == "" && refreshToken == "") {
+  if (authToken == "" && refreshToken == "")
+  {
     // Restart the auth flow
     getDeviceCode(client, http);
-    return;
   }
+}
+
+JsonObject getPresence()
+{
+  DynamicJsonDocument doc(512);
+  String errorStr = "";
+
+  if (authToken != "")
+  {
+    // Create client instance
+    WiFiClientSecure client;
+    // ignore SSL certificate
+    client.setInsecure();
+    // HTTP Client instance
+    HTTPClient http;
+    http.begin(client, "https://graph.microsoft.com/v1.0/me/presence");
+    http.addHeader("Authorization", "Bearer " + authToken);
+    int httpCode = http.GET();
+    if (httpCode == HTTP_CODE_OK)
+    {
+      String payload = http.getString();
+      Serial.print(payload);
+      // Deserialize the JSON document
+      DeserializationError error = deserializeJson(doc, payload);
+      if (error)
+      {
+        errorStr = "Presence GET - Parse Error";
+        Serial.println(errorStr);
+      }
+      else
+      {
+        Serial.println("Presence GET - Success");
+      }
+    }
+    http.end();
+  }
+  else
+  {
+    errorStr = "Presence GET - Auth Token Missing";
+    Serial.print(errorStr);
+  }
+
+  JsonObject result = doc.as<JsonObject>();
+
+  if (errorStr != "") {
+    result["error"] = errorStr;
+  }
+
+  return result;
 }
 
 // Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
 void setup()
 {
+  // initialize GPIO 16 as an output
+  pinMode(LED, OUTPUT);
+  
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   // Initialize SD library
-  while (!SD.begin(SDFILE_PIN_CS)) {
+  while (!SD.begin(SDFILE_PIN_CS))
+  {
     Serial.println("Failed to initialize SD library");
     delay(1000);
   }
 
   // Load config file
-  loadConfiguration(SDFILE_NAME);
-  
-  // Start WIFI connection
-  startWifiConnection();
-  
-  // Create client instance
-  WiFiClientSecure client;
-  
-  // ignore SSL certificate
-  client.setInsecure();
-  
-  // HTTP Client instance
-  HTTPClient http;
-  
-  // Get Device Code
-  getDeviceCode(client, http);
+  loadConfiguration(FILE_NAME);
+
+  if (ssid != "" && password != "")
+  {
+    // Start WIFI connection
+    startWifiConnection();
+
+    // Create client instance
+    WiFiClientSecure client;
+
+    // ignore SSL certificate
+    client.setInsecure();
+
+    // HTTP Client instance
+    HTTPClient http;
+
+    // Get Device Code
+    getDeviceCode(client, http);
+  }
 }
 
 // Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
 void loop()
 {
-  Serial.println(authToken);
-  Serial.println(refreshToken);
-  Serial.println(expiresIn);
-  delay(30 * 1000);
+  if (authToken != "" && refreshToken != "")
+  {
+    while (expiresIn > 0)
+    {
+      expiresIn -= POLLING_INTERVAL;
+      JsonObject presence = getPresence();
+      if (!presence.containsKey("error"))
+      {
+        String activity = presence["activity"].as<String>();
+        if (activity == "DoNotDisturb" ||  activity == "InACall" || activity == "InAConferenceCall" || activity == "InAMeeting" || activity == "Presenting") {
+          Serial.println("Turn On Air signal!");
+          digitalWrite(LED, HIGH);
+        } else {
+          Serial.println("Turn Off Air signal!");
+          digitalWrite(LED, LOW);
+        }
+      }
+      delay(POLLING_INTERVAL * 1000);
+    }
+
+    // Get new auth token / refresh token here
+  }
 }
